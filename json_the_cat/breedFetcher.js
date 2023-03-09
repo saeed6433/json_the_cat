59083c59c765d96a808d19521776e21df66a3530
Saeed <62859084+saeed6433@users.noreply.github.com>
2023-03-08 21:06:08 -0500

    Update breedFetcher.js
    corrected api address

--- json_the_cat/breedFetcher.js
+++ json_the_cat/breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const breedName = process.argv.slice(2);
-request(`https://api.thecaapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   //console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
